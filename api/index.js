636cf68b30889f664bd34f9822058f65fb7ca89e
gamitej <gamitejpratapsingh998@gmail.com>
2022-05-20 16:01:04 +0530

    authRoute & user register done

--- api/index.js
+++ api/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require('./routes/auth')
 
 dotenv.config();
+
+// To use json req 
+app.use(express.json())
 
 // Connect To MongoDB Server
 mongoose
@@ -14,11 +18,14 @@ mongoose
   .then(()=>{console.log("Connected To MongoDB Server ...")})
   .catch((err) => console.log(err));
 
+// authRoute - auth.js
+app.use('/api/auth',authRoute)
+
 app.use("/", (req, res) => {
   console.log("Welcome To My Blog!");
 });
 
 // Port Running
-app.listen("5000", () => {
+app.listen("5001", () => {
   console.log("Backend Is Up & Running ...");
 });
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,8 +4,12 @@ const User = require("../models/User");
 // Register
 
 router.post("/register", async (req, res) => {
+  // To know the req we are getting from client
+  console.log(req.dody)
+
   // Whenever Using async func always use try & catch
   try {
+
     //const newUser = new User(req.body) -> when u want to send everything use req.body
     // else
     const newUser = new User({
@@ -20,3 +24,5 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+module.exports = router
